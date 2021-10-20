e7ae7a12fac601ad40d00dbd9285379dbc670744
Gökhan Öztürk <gokhan@eg-ozturk.com>
2021-10-20 23:47:59 +0300

    Initial commit with auto publish to nuget workflow

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # csharp-htk
-A set of convenience utils for C#
+
+A set of convenience utils for C#.
+
+
+# Features
+
+- Debug via Slack using `Htk.Utils.Debuggers.SlackDebug`.
+- Debug via writing to local file using `Htk.Utils.Debuggers.FileDebug`. Certifiably awesome, fast, and secure.
+
+
+# Setters & Methods
+- `Htk.Utils.Debuggers.FilePath` You can set a default file path for `FileDebug` method.
+- `Htk.Utils.Debuggers.SlackWebhookUrl` Set yout Slack Webhook URL. `SlackDebug` method uses this URL.
+- `Htk.Utils.Debuggers.FileDebug(string text, string fileName = DEFALT_FILE_PATH)` uses `FilePath` if `fileName` is omited.
+- `Htk.Utils.Debuggers.FileDebug(object obj, string fileName = DEFALT_FILE_PATH)` dumps given object to file using JSON Serialization. uses `FilePath` if `fileName` is omited.
+- `Htk.Utils.Debuggers.SlackDebug(string text)` Sends the given string to Slack.
+- `Htk.Utils.Debuggers.SlackDebug(string text, string label)` Sends the given string to Slack. Format: label: text
+- `Htk.Utils.Debuggers.SlackDebug(object obj)` Sends the given object as JSON to Slack.
+- `Htk.Utils.Debuggers.SlackDebug(object obj, string label)` Sends the given object as JSON to Slack. Format: label: JSON Dump of object
+
+**NOTE:** `FileDebug` has `fdebug` alias and `SlackDebug` has `slack_debug` alias for easy usage.
+
+# How To Use This Awesome?
+
+1. Install via nuget.
+1. (**Alternative install via clone**) Install via local clone: clone this repository into a directory named `htk`  
+    SSH: `git clone git@github.com:hacktoolkit/csharp-htk.git htk`  
+    HTTPS: `git clone https://github.com/hacktoolkit/csharp-htk.git`
+1. Add your [Slack incoming webhook](https://slack.com/apps/A0F7XDUAZ-incoming-webhooks) URL.
+1. Check your Slack to verify that the message was posted. If not, perhaps your token was wrong, or the Slack integration was disabled.
+    ![image](https://user-images.githubusercontent.com/422501/61013274-e65e1e00-a336-11e9-90aa-44a6fd1e217c.png)  
+    (Alternative link to screenshot above: https://cl.ly/436cfb4383a2)
+1. Profit!
+
+# Authors and Maintainers
+
+- [Hacktoolkit](https://github.com/hacktoolkit)
+- [Jonathan Tsai](https://github.com/jontsai)
+- [Gökhan Öztürk](https://github.com/Quanthir)
+
+# License
+
+MIT. See `LICENSE.md`
---
+++ Utils/Debuggers.cs
@@ -0,0 +1,158 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Htk.Utils
+{
+    public static class Debuggers
+    {
+        private const string DEFAULT_FILE_PATH = "fdebug.log";
+
+        public static int Counter { get; set; } = 0;
+
+        public static string FilePath { get; set; } = DEFAULT_FILE_PATH;
+
+        /// <summary>
+        /// Set the Slack webhook URL
+        /// </summary>
+        public static string SlackWebhookUrl { get; set; }
+
+        /// <summary>
+        /// Writes given string to file.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="fileName"></param>
+        public static void FileDebug(string text, string fileName = null)
+        {
+            string time = $"{DateTime.Now.ToString("s")}";
+
+            using (StreamWriter sw = new StreamWriter(fileName ?? FilePath))
+            {
+                sw.WriteLine($">>>>>>>>>> FDEBUG {time} {++Counter} <<<<<<<<<<");
+                sw.WriteLine(text);
+                sw.WriteLine($">>>>>>>>>> FDEBUG {time} {Counter} <<<<<<<<<<");
+            }
+        }
+
+        /// <summary>
+        /// Writes given object to file as JSON.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="fileName"></param>
+        public static void FileDebug(object obj, string fileName = null)
+        {
+            FileDebug(JsonConvert.SerializeObject(obj), fileName);
+        }
+
+        /// <summary>
+        /// Sends given string to Slack.
+        /// </summary>
+        /// <param name="text">Text to send</param>
+        public static Slack SlackDebug(string text)
+        {
+            return Slack.Send(SlackWebhookUrl, text);
+        }
+
+        /// <summary>
+        /// Sends given string to Slack.
+        /// </summary>
+        /// <param name="text">Text to send</param>
+        /// <param name="label">Label for the text.</param>
+        public static Slack SlackDebug(string text, string label)
+        {
+            return Slack.Send(SlackWebhookUrl, $"{label}: {text}");
+        }
+
+        /// <summary>
+        /// Sends given object to Slack as JSON.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Slack Instance</returns>
+        public static Slack SlackDebug(object obj)
+        {
+            return Slack.Send(SlackWebhookUrl, JsonConvert.SerializeObject(obj));
+        }
+
+        /// <summary>
+        /// Sends given object to Slack as JSON.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="label">Label for the text.</param>
+        /// <returns>Slack Instance</returns>
+        public static Slack SlackDebug(object obj, string label)
+        {
+            return Slack.Send(SlackWebhookUrl, $"{label}: {JsonConvert.SerializeObject(obj)}");
+        }
+
+        // Aliases
+
+#pragma warning disable IDE1006 // Naming Styles
+
+        /// <summary>
+        /// Writes given string to file.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="fileName"></param>
+        public static void fdebug(string text, string fileName = null)
+        {
+            FileDebug(text, fileName);
+        }
+
+        /// <summary>
+        /// Writes given object to file as JSON.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="fileName"></param>
+        public static void fdebug(object obj, string fileName = null)
+        {
+            FileDebug(JsonConvert.SerializeObject(obj), fileName);
+        }
+
+        /// <summary>
+        /// Sends given text to Slack.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>Slack Instance</returns>
+        public static Slack slack_debug(string text)
+        {
+            return Slack.Send(SlackWebhookUrl, text);
+        }
+
+        /// <summary>
+        /// Sends given text to Slack.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="label"></param>
+        /// <returns>Slack Instance</returns>
+        public static Slack slack_debug(string text, string label)
+        {
+            return Slack.Send(SlackWebhookUrl, $"{label}: {text}");
+        }
+
+        /// <summary>
+        /// Sends given object to Slack as JSON.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Slack Instance</returns>
+        public static Slack slack_debug(object obj)
+        {
+            return Slack.Send(SlackWebhookUrl, JsonConvert.SerializeObject(obj));
+        }
+
+        /// <summary>
+        /// Sends given object to Slack as JSON.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="label"></param>
+        /// <returns>Slack Instance</returns>
+        public static Slack slack_debug(object obj, string label)
+        {
+            return Slack.Send(SlackWebhookUrl, $"{label}: {JsonConvert.SerializeObject(obj)}");
+        }
+#pragma warning restore IDE1006 // Naming Styles
+    }
+}
